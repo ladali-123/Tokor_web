7a4573599bc800147810e1da3f03e4a81ad709b0
Ladali Kumari <ladalikumari683@gmail.com>
2026-06-12 15:20:05 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import WhyChoose from './components/WhyChoose'
-import Announce from './components/Announce';
-import Customer from './components/Customer';
-import Contact from './components/Contact';
+import Announce from './components/Announce'
+import Customer from './components/Customer'
+import Contact from './components/Contact'
 import Review from './components/Review'
 import Footer from './components/Footer'
+
 function App() {
-return (
+  return (
     <>
-    <Announce/>
-    <Navbar/>
-    <Home/>
-    <About/>
-    <WhyChoose/>
-    <Customer/>
-    <Contact/>
-    <Review/>
-    <Footer/>
+      <Announce />
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/why-choose" element={<WhyChoose />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/review" element={<Review />} />
+      </Routes>
+
+      <Footer />
     </>
   )
 }
